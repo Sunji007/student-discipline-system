0e1776e4fac9e7b53c44993fb08c62987cc7478e
Sunji007 <sun45329@gmail.com>
2026-06-28 01:55:14 +0700

    added Thai translation for authentication errors

---
+++ lang/th/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    | Translation: Thai
+    */
+
+    'failed' => 'ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง',
+    'password' => 'รหัสผ่านที่ป้อนไม่ถูกต้อง',
+    'throttle' => 'มีการพยายามเข้าสู่ระบบมากเกินไป กรุณาลองใหม่อีกครั้งในอีก :seconds วินาที',
+
+];
